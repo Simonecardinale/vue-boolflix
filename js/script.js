ce0e7d83aa217d22838c6178b228d31ed08357c8
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-10 19:59:31 +0100

    Milestone 1 and 3 complete. Milestone 2 WIP

--- js/script.js
+++ js/script.js
@@ -2,18 +2,45 @@ var app = new Vue({
     el: '#app',
     data: {
         film: [],
+        series:[],
         query: '',
-        poster: "https://image.tmdb.org/t/p/w342" 
+        poster: "https://image.tmdb.org/t/p/w342",
+        votoStella: [],
     },
     methods: {
         searchFilm(){
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=170b9b6ee3a93a04400d81014d1e9315&language=it-IT', {
+
+            //film
+
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=170b9b6ee3a93a04400d81014d1e9315&language=it-IT' , {
             params: {
-                query: this.query
+                query: this.query,
             }
             }).then ((result)=> {
                 this.film = result.data.results;
+                for(let k in result.data.results){
+                    this.votoStella.push(Math.trunc(Math.ceil((result.data.results[k].vote_average * 0.5))));
+                }
             });
-        }
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=170b9b6ee3a93a04400d81014d1e9315&language=it-IT&page=1&include_adult=false', {
+                params: {
+                    query: this.query,
+                }
+            }).then((result)=> {
+                this.series = result.data.results;
+                console.log(this.film);
+            });
+        },
     }
 });
+
+
+// function  callApi(link) {
+//     axios.get(link, { params:
+//         {
+//         query: this.query,
+//     }
+//     }).then((result)=> {
+//         this.film = result.data.results;
+//     })
+// }
